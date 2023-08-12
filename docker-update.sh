334dcc0c104279cc6e8fbb0504dac767d364f497
Austin Kirsch <37373320+kirsch33@users.noreply.github.com>
2023-08-12 18:25:46 -0400

    Update docker-update.sh

--- docker-update.sh
+++ docker-update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a containers=("caddy" "unifi" "jackett" "overseerr" "tautulli" "radarr" "sonarr" "bazarr" "transmission" "plex" "fail2ban" "bitwarden" "bookstack_db" "bookstack")
+declare -a containers=("overseerr" "tautulli" "radarr" "sonarr" "bazarr" "audiobookshelf" "plex" "fail2ban" "bitwarden" "bookstack_db" "bookstack" "pmm" "prowlarr" "sabnzbd" "gluetun" "qbit" "influxdb" "grafana")
 
 arraylength=${#containers[@]}
 
@@ -31,24 +31,24 @@ function update_selective {
 }
 
 function update_container {
-                (/usr/bin/docker stop "$1")
-                (/usr/bin/docker rm "$1")
+        (/usr/bin/docker stop "$1")
+        (/usr/bin/docker rm "$1")
 
-                (/usr/bin/docker system prune -f -a)
+        (/usr/bin/docker system prune -f -a)
 
-                if [ "$1" != "bookstack_db" ] ; then
-                        (/usr/local/bin/docker-compose -f /var/docks/"$1"/docker-compose.yml up -d)
-                fi
+        if [[ "$1" != @(bookstack_db|gluetun|influxdb) ]] ; then
+                (/usr/local/bin/docker-compose -f /var/docks/"$1"/docker-compose.yml up -d)
+        fi
 
-                (/usr/bin/docker system prune -f -a)
+        (/usr/bin/docker system prune -f -a)
 }
 
 echo "Update all containers?"
 
-select yn in "Yes" "No";
+select yn in "Yes" "No"; 
 do
-        case $yn in
+        case $yn in 
                 Yes ) update_all; break;;
-                No ) update_selective;;
+                No ) update_selective;; 
         esac
 done
